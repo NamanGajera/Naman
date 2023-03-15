7d045bd5a4d1355f51091d9cf3e5f307824e883d
NamanGajera <125533942+NamanGajera@users.noreply.github.com>
2023-03-15 15:16:25 +0530

    Add files via upload

---
+++ C++ Daily Task/Star_Pattern(2).cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	for(int i =  0 ; i < 5 ; i++)
+	{
+		for(int j = 0 ; j < i+1 ;j++)
+		{
+			cout<<" * ";
+		}
+		cout<<"\n";
+	}
+	for(int i =  0 ; i < 5 ; i++)
+	{
+		
+		for(int j = 5 ; j > i ;j--)
+		{
+			cout<<" * ";
+		}
+		cout<<"\n";
+	}
+	
+}
