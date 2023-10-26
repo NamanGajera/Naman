9ce3a56724063bcb5e3383dc3b9c92964620a43f
Gajera Naman <125533942+NamanGajera@users.noreply.github.com>
2023-10-26 21:24:45 +0530

    Add files via upload

---
+++ Assignment/Dart Assignment/Que1.dart
@@ -0,0 +1,10 @@
+void main() {
+  print('----Name-----');
+  print('Gajera Naman');
+  print('----Birth date----');
+  print('23 Feb 2003');
+  print('----Age----');
+  print('20');
+  print('----Address----');
+  print('At.- Dhargani, Ta.-Dhari, Dis.- Amreli');
+}
